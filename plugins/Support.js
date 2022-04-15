5a9ddc54c557cb5512584d43b0113eb1314c1f15
DEVILL-MASCOT <102029388+DEVILL-MASCOT@users.noreply.github.com>
2022-04-15 16:09:24 +0530

    Update Support.js

--- plugins/Support.js
+++ plugins/Support.js
@@ -2,9 +2,7 @@ let handler = async m => {
 
     const grouplink = `https://chat.whatsapp.com/JdCxJkOzJn38TRYq5Q5evp`
 
-    m.reply(grouplink, m.sender) 
-
-    m.reply('✅ Support group link has been successfully sent to your private')
+    m.reply(grouplink) 
 
 }
 
